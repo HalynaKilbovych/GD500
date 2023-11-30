10727f66243f2bb3070899f3dc173295513dd891
STIg89 <baza.trainee@gmail.com>
2023-11-30 17:14:07 +0200

    fix

--- js/slider.js
+++ js/slider.js
@@ -1,48 +1,51 @@
 // Створення інстансу Swiper для слайдера новин
-const swiper = new Swiper('#newsSwiper', {
-  direction: 'horizontal',
+const swiper = new Swiper("#newsSwiper", {
+  direction: "horizontal",
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  breakpoints: { 
+  breakpoints: {
     320: { slidesPerView: 1 },
     768: { slidesPerView: 2 },
     1200: { slidesPerView: 2.4 },
     1600: { slidesPerView: 3 },
   },
   spaceBetween: 40,
-  grabCursor: true, 
-  effect: 'coverflow',
+  grabCursor: true,
+  effect: "coverflow",
   coverflowEffect: {
     rotate: 0,
     slideShadows: true,
-    depth: 5, 
+    depth: 5,
   },
 });
 
-
-const videoSwiper = new Swiper('#videoSwiper', {
+const videoSwiper = new Swiper("#newVideoSwiper", {
   slidesPerView: 1,
   spaceBetween: 20,
   navigation: {
-    nextEl: '.video-swiper-button-next',
-    prevEl: '.video-swiper-button-prev',
+    nextEl: ".video-swiper-button-next",
+    prevEl: ".video-swiper-button-prev",
   },
 });
 
-document.querySelectorAll('.video-thumbnail').forEach(function (thumbnail, index) {
-  thumbnail.addEventListener('click', function () {
-    var videoSrc = thumbnail.getAttribute('data-src');
-    playVideo(videoSrc, index);
+document
+  .querySelectorAll(".video-thumbnail")
+  .forEach(function (thumbnail, index) {
+    thumbnail.addEventListener("click", function () {
+      var videoSrc = thumbnail.getAttribute("data-src");
+      playVideo(videoSrc, index);
+    });
   });
-});
-
 
 function playVideo(src, index) {
-  var videoContainer = document.getElementById('video-container');
-  videoContainer.innerHTML = '<video controls width="600" height="400" id="video"><source src="' + src + '" type="video/mp4">Your browser does not support the video tag.</video>';
-  videoContainer.style.display = 'block';
-  videoSwiper.slideTo(index); 
-} 
+  var videoContainer = document.getElementById("video-container");
+  videoContainer.innerHTML =
+    '<video controls width="600" height="400" id="video"><source src="' +
+    src +
+    '" type="video/mp4">Your browser does not support the video tag.</video>';
+  videoContainer.style.display = "block";
+  videoSwiper.slideTo(index);
+}
