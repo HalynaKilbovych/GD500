f0740c17a8b610e34ae57832c4d6f22c85d5a449
Halyna Kilbovych <108695210+HalynaKilbovych@users.noreply.github.com>
2023-11-30 21:29:37 +0200

    fix

--- js/menu.js
+++ js/menu.js
@@ -19,33 +19,3 @@
   })();
 
 
-
-
-  document.addEventListener('DOMContentLoaded', function () {
-    
-  
-    // Додаємо подію на клік по картинці в слайді
-    document.querySelectorAll('.swiper-slide').forEach(function (slide) {
-      slide.addEventListener('click', function () {
-        var videoSrc = slide.querySelector('.video-thumbnail').getAttribute('data-src');
-        playVideo(videoSrc);
-      });
-    });
-  
-    // Функція для відтворення відео
-    function playVideo(src) {
-      // Отримуємо контейнер відео
-      var videoContainer = document.getElementById('video-container');
-  
-      // Встановлюємо відео
-      videoContainer.innerHTML = '<video controls width="600" height="400" id="video"><source src="' + src + '" type="video/mp4">Your browser does not support the video tag.</video>';
-  
-      // Приховуємо інші слайди
-      document.querySelectorAll('.swiper-slide').forEach(function (slide) {
-        slide.style.display = 'none';
-      });
-  
-      // Відображаємо контейнер відео
-      videoContainer.style.display = 'block';
-    }
-  });
--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-// Створення інстансу Swiper для слайдера новин
+
 const swiper = new Swiper("#newsSwiper", {
   direction: "horizontal",
   loop: true,
@@ -21,35 +21,5 @@ const swiper = new Swiper("#newsSwiper", {
   },
 });
 
-const videoSwiper = new Swiper("#newVideoSwiper", {
-  slidesPerView: 2,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".video-swiper-button-next",
-    prevEl: ".video-swiper-button-prev",
-  },
-  coverflowEffect: {
-    rotate: 0,
-    slideShadows: true,
-    depth: 5,
-  },
-});
 
-document
-  .querySelectorAll(".video-thumbnail")
-  .forEach(function (thumbnail, index) {
-    thumbnail.addEventListener("click", function () {
-      var videoSrc = thumbnail.getAttribute("data-src");
-      playVideo(videoSrc, index);
-    });
-  });
 
-function playVideo(src, index) {
-  var videoContainer = document.getElementById("video-container");
-  videoContainer.innerHTML =
-    '<video controls width="600" height="400" id="video"><source src="' +
-    src +
-    '" type="video/mp4">Your browser does not support the video tag.</video>';
-  videoContainer.style.display = "block";
-  videoSwiper.slideTo(index);
-}
