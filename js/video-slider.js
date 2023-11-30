aaacbb1b3518b309e563e87dd1c6f9b8294f58fe
Halyna Kilbovych <108695210+HalynaKilbovych@users.noreply.github.com>
2023-12-01 00:28:58 +0200

    add video block

--- js/video-slider.js
+++ js/video-slider.js
@@ -7,23 +7,3 @@ const videoSwiper = new Swiper("#newVideoSwiper", {
     },
   });
 
-
-
-//   document
-//   .querySelectorAll(".video-thumbnail")
-//   .forEach(function (thumbnail, index) {
-//     thumbnail.addEventListener("click", function () {
-//       var videoSrc = thumbnail.getAttribute("data-src");
-//       playVideo(videoSrc, index);
-//     });
-//   });
-
-// function playVideo(src, index) {
-//   var videoContainer = document.getElementById("video-container");
-//   videoContainer.innerHTML =
-//     '<video controls width="600" height="400" id="video"><source src="' +
-//     src +
-//     '" type="video/mp4">Your browser does not support the video tag.</video>';
-//   videoContainer.style.display = "block";
-//   videoSwiper.slideTo(index);
-// }
